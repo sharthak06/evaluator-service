b0e9ba15b6878a92717861ca0abaf58931d5adaf
sharthak singh <sharthaksingh06@gmail.com>
2025-07-13 23:13:41 +0530

    Basic setup of ioredis and Bullmq

---
+++ src/config/redisConfig.ts
@@ -0,0 +1,10 @@
+import { Redis } from "ioredis";
+import  ServerConfig  from './serverConfig.js'
+
+const redisConfig = {
+    port: ServerConfig.REDIS_PORT,
+    host: ServerConfig.REDIS_HOST
+};
+const redisConnection = new Redis(redisConfig);
+
+export default redisConnection;
